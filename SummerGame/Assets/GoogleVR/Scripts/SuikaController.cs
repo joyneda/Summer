9d923e24bae943c629650fa18ad607820596ebba
長谷部直起 <hasebenaoki@kaguwlan217242.ed.tus.ac.jp>
2018-06-23 13:08:56 +0900

    スイカの追加とprefab化

---
+++ SummerGame/Assets/GoogleVR/Scripts/SuikaController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuikaController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float direction;
+	// Use this for initialization
+	void Start () {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Pakkan();
+        }
+	}
+
+    void Pakkan(){
+        rb.constraints = RigidbodyConstraints.None;
+        rb.AddForce(new Vector3(direction, 2, 0), ForceMode.Impulse);
+        rb.AddTorque(0, 0, -direction);
+    }
+}
