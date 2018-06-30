4e41f6f344c1ea5cb0289ba66cad0befebee5c05
長谷部直起 <hasebenaoki@kaguwlan221066.ed.tus.ac.jp>
2018-06-30 17:03:38 +0900

    ルーム１の途中結果

---
+++ SummerGame/Assets/Scripts/SlashController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlashController : MonoBehaviour {
+
+    public GameObject Obj;
+    private GameObject mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(GvrControllerInput.Accel);
+        if(Mag()>3.0f){
+            Instantiate(Obj, mainCamera.transform.position, Quaternion.identity);
+        }
+            
+        }
+	
+
+    double Mag(){
+        Vector3 accel = GvrControllerInput.Accel;
+        return 0.0f;
+    }
+}
---
+++ SummerGame/Assets/Scripts/SlashingController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlashingController : MonoBehaviour {
+
+    private GameObject mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = Camera.main.gameObject;
+        Rigidbody rb = this.gameObject.GetComponent<Rigidbody>();
+        rb.AddForce(mainCamera.transform.forward,ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
