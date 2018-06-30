24e09fd6c2a4000498aa9b089ef2e354d560fa47
小金井宏樹 <koganeihiroki@kaguwlan221030.ed.tus.ac.jp>
2018-06-30 20:15:27 +0900

    爆発

---
+++ SummerGame/Assets/Scripts/BombDestroy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class BombDestroy : MonoBehaviour
+{
+    public GameObject explosion;
+    Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("zangeki"))
+        {
+            Destroy(gameObject);
+            Instantiate(explosion, this.transform.position, Quaternion.identity);
+        }
+    }
+}
