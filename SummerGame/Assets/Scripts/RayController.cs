daf21d41adef901d4e97bbaba30d14db85a843da
長谷部直起 <hasebenaoki@kaguwlan209036.ed.tus.ac.jp>
2018-07-13 15:32:23 +0900

    途中経過

---
+++ SummerGame/Assets/Scripts/RayController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayController : MonoBehaviour {
+
+    private UnityEngine.UI.Image aim;
+    private GameObject Pointer;
+	// Use this for initialization
+	void Start () {
+        aim = this.GetComponent<UnityEngine.UI.Image>();
+        Pointer = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = new Ray(Pointer.transform.position, Pointer.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction, Color.red);
+	}
+}
