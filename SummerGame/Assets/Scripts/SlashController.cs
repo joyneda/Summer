01dd351208196cb8cd79f18a9c67b588b290ba83
長谷部直起 <hasebenaoki@kaguwlan217175.ed.tus.ac.jp>
2018-07-14 15:08:07 +0900

    こんなもんかな

--- SummerGame/Assets/Scripts/SlashController.cs
+++ SummerGame/Assets/Scripts/SlashController.cs
@@ -7,19 +7,40 @@ public class SlashController : MonoBehaviour {
     public GameObject Obj;
     private GameObject mainCamera;
     public GameObject ken;
+    public bool lapse;
+    public float coolTime;
+    private float lapseTime;
 
 	// Use this for initialization
 	void Start () {
         mainCamera = Camera.main.gameObject;
+
+
+        lapseTime = 0;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Debug.Log(GvrControllerInput.Accel);
-        if(Mag()>12.0f){
-            Instantiate(Obj, ken.transform.position, Quaternion.identity);
+
+        if (lapse == true)
+        {
+            if (Mag() > 12.0f)
+            {
+                Instantiate(Obj, ken.transform.position, Quaternion.identity);
+
+
+                lapse = false;
+            }
         }
-            
+        if(lapse == false){
+            lapseTime += Time.deltaTime;
+            if(lapseTime > coolTime){
+                lapse = true;
+                lapseTime = 0;
+            }
+        }
         }
 	
 
